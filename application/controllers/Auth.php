cc817d0b4ebc1fcb39551f6791dffedd89fac011
Yu-Hsin Sean Wang <katnapped@hotmail.com>
2015-04-10 17:13:06 -0700

    Added login form and Auth controller.

---
+++ application/controllers/Auth.php
@@ -0,0 +1,12 @@
+<?php
+
+class Auth extends Application {
+   function __construct() {
+     parent::__construct();
+     $this->load->helper('url');
+   }
+   function index() {
+     $this->data['pagebody'] = 'login';
+     $this->render();
+   }
+ } 
